bc78e682105c53d683640703b076b896df896057
jcmestti <jcmestti@gmail.com>
2022-04-16 17:42:49 -0300

    Checking answers

---
+++ app.js
@@ -0,0 +1,16 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let scores = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            scores += 25;
+        }
+    })
+    console.log(scores);
+});
